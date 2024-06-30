f0ed9afa2bd414cdf331a992b7e4c31e2200f6de
syed-bilalahmed <syedbilalahmed5292@gmail.com>
2024-06-30 00:29:53 -0700

    add fields in Todo.models file

--- model/todo/todo.model.js
+++ model/todo/todo.model.js
@@ -1,4 +1,21 @@
 import mongoose, { mongo } from 'mongoose';
 
-const todoSchema = new mongoose.Schema({}, { timestamps: true });
+const todoSchema = new mongoose.Schema({
+
+    content: {
+         typeof: String,
+         required: true },
+
+    complete: {
+             type: Boolean,
+             default: false,     
+    },
+    createdBy:{
+        type:mongoose.Schema.typeofObjectId,
+        ref:"User",
+
+    }
+  
+}, 
+{ timestamps: true });
 export const todo = mongoose.model('Todo', todoSchema);
